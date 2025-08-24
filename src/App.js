f407dec7a2a4c589a56a2f29d7ef825d5edc4070
huss313 <hussrash@gmail.com>
2025-08-24 17:20:20 +0300

    update4

--- src/App.js
+++ src/App.js
@@ -88,13 +88,12 @@ const RevealOnScroll = ({ children }) => {
   const [isVisible, setVisible] = useState(false);
   const domRef = useRef();
 
-useEffect(() => {
-  fetch('https://vent-ijwk.onrender.com/log-visitor')
-    .then(res => res.text())
-    .then(console.log)
-    .catch(console.error);
-}, []);
-
+  useEffect(() => {
+    fetch('https://vent-ijwk.onrender.com/log-visitor')
+     .then(res => res.text())
+     .then(console.log)
+     .catch(console.error);
+  }, []);
 
   useEffect(() => {
     const observer = new IntersectionObserver(entries => {
